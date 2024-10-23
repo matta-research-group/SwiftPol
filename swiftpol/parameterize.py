24f4cdff9c4ca5bc53b96dd8786d55c5ec0a64c4
Hannah <hannahturney0@gmail.com>
2024-10-23 12:17:55 +0100

    removed double espaloma and nagl dependency

--- swiftpol/parameterize.py
+++ swiftpol/parameterize.py
@@ -1,11 +1,13 @@
 import rdkit
 from rdkit import Chem
 from rdkit.Chem import AllChem
-import espaloma_charge as espcharge
-from espaloma_charge.openff_wrapper import EspalomaChargeToolkitWrapper
+#try:
+    #import espaloma_charge as espcharge
+#except ImportError:
+    #raise ImportError("The espaloma package is not installed. You will not be able to use EspalomaCharge.")
 from openff import toolkit
 from openff.toolkit.topology import Molecule
-from openff.toolkit.utils.nagl_wrapper import NAGLToolkitWrapper
+#from openff.toolkit.utils.nagl_wrapper import NAGLToolkitWrapper
 
 
 def charge_polymer(polymer, charge_scheme):
@@ -29,9 +31,19 @@ def charge_polymer(polymer, charge_scheme):
         openff_chain.assign_partial_charges("am1bcc")
         return openff_chain.partial_charges
     elif charge_scheme == 'espaloma':
+        try:
+            import espaloma_charge as espcharge
+        except ImportError:
+            raise ImportError("The package espaloma-charge is not installed. You will not be able to use EspalomaCharge.")
         chain_h = Chem.AddHs(polymer)
         return espcharge.charge(chain_h)
-    elif charge_scheme == 'NAGL' and toolkit.__version__ >= '0.16.0':
+    elif charge_scheme == 'NAGL' and toolkit.__version__ < '0.16.0':
+        raise ModuleNotFoundError("Installed version of openff-toolkit is below what is required to use NAGL. Please update to v.0.16.0")
+    elif charge_scheme == 'NAGL' and toolkit.__version__ == '0.16.0':
+        try:
+            from openff.toolkit.utils.nagl_wrapper import NAGLToolkitWrapper
+        except:
+            raise ImportError("The package openff-nagl is not installed. You will not be able to use NAGL.")
         chain_h = Chem.AddHs(polymer)
         openff_chain = Molecule.from_rdkit(chain_h)
         ntkw = NAGLToolkitWrapper()
@@ -41,6 +53,8 @@ def charge_polymer(polymer, charge_scheme):
     else:
         raise AttributeError("This function takes either 'AM1_BCC', 'NAGL', or 'espaloma' as charge_scheme input")
     
+
+
     
 
 
@@ -49,10 +63,9 @@ def forcefield_with_charge_handler(molecule, charge_method, forcefield = "openff
     Create a forcefield with a charge handler for a given molecule and charge method. The function can handle both individual molecules and ensembles of molecules.
 
     Parameters:
-    molecule: An RDKit molecule object or SwiftPol Ensemble for which the forcefield is to be created.
+    molecule: An RDKit molecule object or a list of RDKit molecule objects for which the forcefield is to be created.
     charge_method: A string that specifies the charge method to be used for the molecule.
-    forcefield: A string that specifies the OpenFF forcefield to be used. Default is "openff-2.2.0.offxml". IF a non-OpenFF or bespoke force field is being used,
-    the user can specify the path to the force field file (format = SMIRNOFF XML).
+    forcefield: A string that specifies the forcefield to be used. Default is "openff-2.2.0.offxml".
     ensemble: A boolean that specifies whether the input molecule is an ensemble of molecules. Default is False.
 
     Returns:
@@ -74,73 +87,25 @@ def forcefield_with_charge_handler(molecule, charge_method, forcefield = "openff
         openff_molecule = Molecule.from_rdkit(molecule)
         charges = charge_polymer(molecule, charge_method)
         openff_molecule.partial_charges = charges * unit.elementary_charge
-
-        #Create library charge type from openff_molecule
+        
+        # Add charges to OpenFF force field
         library_charge_type = LibraryChargeHandler.LibraryChargeType.from_molecule(openff_molecule)
 
         # Pull base force field
-        forcefield = ForceField(forcefield) #If forcefield is a string, it will be treated as a file path 
-        # Add charges to OpenFF force field
+        forcefield = ForceField(forcefield)
         forcefield["LibraryCharges"].add_parameter(parameter=library_charge_type)
         
     elif ensemble==True:
         # Pull base force field
         forcefield = ForceField(forcefield)
-        #Remove duplicate chains
-        chains = set(molecule.chains)
-        for i in chains:
-            charges = charge_polymer(i.to_rdkit(), charge_method)
-            i.partial_charges = charges * unit.elementary_charge
+        for i in molecule.chain_rdkit:
+            openff_molecule = Molecule.from_rdkit(i)
+            charges = charge_polymer(i, charge_method)
+            openff_molecule.partial_charges = charges * unit.elementary_charge
         
             # Add charges to OpenFF force field
-            library_charge_type = LibraryChargeHandler.LibraryChargeType.from_molecule(i)
+            library_charge_type = LibraryChargeHandler.LibraryChargeType.from_molecule(openff_molecule)
 
             forcefield["LibraryCharges"].add_parameter(parameter=library_charge_type)
     
-    return forcefield
-
-def forcefield_with_residualmonomer(system, charge_method, forcefield):
-    """
-    Add charges to a forcefield for a system with residual monomers.
-
-    This function calculates the charges for lactide and glycolide monomers using the specified charge method, 
-    and adds these charges to the provided forcefield. If the system does not contain any residual monomers, 
-    the function raises an AttributeError.
-
-    Parameters:
-    system (object): An object representing the system. The system should have a 'residual_monomer' attribute 
-                     indicating the number of residual monomers in the system.
-    charge_method (str): The method to use for calculating charges. This should be a string recognized by the 
-                         'charge_polymer' function.
-    forcefield (ForceField): The forcefield to which to add the charges, OpenFF ForceField() object.
-
-    Returns:
-    ForceField: The forcefield with the added charges.
-
-    Raises:
-    AttributeError: If the system does not contain any residual monomers.
-    """
-    import numpy as np
-    from openmm import unit
-    from openff.toolkit.topology import Molecule
-    from openff.toolkit.typing.engines.smirnoff import ForceField
-    from openff.toolkit.typing.engines.smirnoff.parameters import LibraryChargeHandler
-    from swiftpol.parameterize import charge_polymer
-    if system.residual_monomer > 0:
-        charges_lac = charge_polymer(Chem.MolFromSmiles('C[C@@H](C(=O)[OH])O'), charge_method)
-        charges_gly = charge_polymer(Chem.MolFromSmiles('OC(=O)CO'), charge_method)
-        lac = Molecule.from_rdkit(Chem.MolFromSmiles('C[C@@H](C(=O)[OH])O'))
-        gly = Molecule.from_rdkit(Chem.MolFromSmiles('OC(=O)CO'))
-        lac.partial_charges = charges_lac * unit.elementary_charge
-        gly.partial_charges = charges_gly * unit.elementary_charge
-            
-        # Add charges to OpenFF force field
-        library_charge_type = LibraryChargeHandler.LibraryChargeType.from_molecule(lac)
-        forcefield["LibraryCharges"].add_parameter(parameter=library_charge_type)
-        library_charge_type = LibraryChargeHandler.LibraryChargeType.from_molecule(gly)
-        forcefield["LibraryCharges"].add_parameter(parameter=library_charge_type)
-        
-    else:
-        raise AttributeError("The system does not contain any residual monomer")
-
     return forcefield
