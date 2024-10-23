dff3cc96f86db9184281a9aa4ebc4f7bfd0f7efb
Hannah <hannahturney0@gmail.com>
2024-10-23 11:33:53 +0100

    moved plga functions to demo module and debug tests

--- swiftpol/tests/build_tests.py
+++ swiftpol/tests/build_tests.py
@@ -3,29 +3,13 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 import os
 from swiftpol import build 
+from swiftpol import demo
 import numpy as np  # Added for numpy array type checking
 from openff.toolkit.topology import Topology  # Added for topology type checking
 import warnings
 warnings.filterwarnings("ignore")
 
 # Define all test cases
-
-class TestBuildPLGARing(unittest.TestCase):
-    def test_build_PLGA_ring(self):
-        sequence = 'LLGG'
-        reaction = AllChem.ReactionFromSmarts('[I:1][O:2].[I:3][C:4]>>[C:4][O:2].[I:3][I:1]')
-        terminal = 'hydroxyl'
-        polymer, LA_ratio, GA_ratio = build.build_PLGA_ring(sequence, reaction, terminal)
-        
-        # Check if the returned polymer is a RDKit Mol object
-        self.assertTrue(isinstance(polymer, Chem.rdchem.Mol))
-        
-        # Check if the returned ratios are as expected
-        self.assertEqual(LA_ratio, 50.0)
-        self.assertEqual(GA_ratio, 50.0)
-        
-
-
 class TestBuildPolymer(unittest.TestCase):
     def test_build_polymer_ROP(self):
         sequence = 'AABBAABB'
@@ -90,24 +74,7 @@ class TestPDI(unittest.TestCase):
         self.assertAlmostEqual(mn, 69.3, places=1)
         self.assertAlmostEqual(mw, 122.6, places=1)
 
-class TestBlockinessPLGA(unittest.TestCase):
-    def test_blockiness_PLGA(self):
-        sequence = 'GLGLGLGL'
-        blockiness, block_length_G, block_length_L = build.blockiness_PLGA(sequence)
-        
-        # Check if the returned blockiness is float
-        self.assertTrue(isinstance(blockiness, float))
 
-
-        # Check if the returned blockiness, block_length_G, and block_length_L are as expected
-        self.assertAlmostEqual(blockiness, 0.0, places=2)
-        self.assertAlmostEqual(block_length_G, 1.0, places=2)
-        self.assertAlmostEqual(block_length_L, 1.0, places=2)
-        
-        #Test case - PLA sequence
-        sequence_L = 'LLLLLLLL'
-        blockiness_L, block_length_G_2, block_length_L_2 = build.blockiness_PLGA(sequence_L)
-        self.assertTrue(isinstance(blockiness_L, str))
 
 class TestBlockinessGen(unittest.TestCase):
     def test_blockiness_Gen(self):
@@ -127,31 +94,13 @@ class TestBlockinessGen(unittest.TestCase):
         sequence_A = 'LLLLLLLL'
         blockiness_A, block_length_A_2, block_length_B_2 = build.blockiness_gen(sequence_A)
         self.assertTrue(isinstance(blockiness_A, str))
-class TestPLGABuild(unittest.TestCase):
-    def test_plga_build(self):
-        x = build.PLGA_system(80, 10, 1.0, 'ester', 5)
-        
-        self.assertTrue(len(x.chains)==5)
-        self.assertTrue(72 <= round(x.lactide_actual) <= 88)
-        self.assertTrue(9 <= round(x.max_length)<= 11)
-        self.assertTrue(1.5 <= round(x.PDI) <= 5)
-        print(x.PDI)
-        self.assertTrue(x.mean_blockiness==1)
-        
-        x.generate_conformers()
-        self.assertTrue(len(x.chains[0].conformers[0])==len(x.chains[0].atoms))
-        x.charge_system()
-        self.assertTrue(len(x.chains[0].partial_charges)==len(x.chains[0].atoms))
-        from openff.units import unit
-        solv_system = x.solvate_system(resid_monomer = 0.5, salt_concentration = 0.1 * unit.mole / unit.liter)
-        self.assertAlmostEqual(x.residual_monomer,0.5,places=1)
-        self.assertIsNotNone(solv_system)  
+
         
 #Test calculate box components
 class TestCalculateBoxComponents(unittest.TestCase):
     def test_calculate_box_components(self):
         # Create a PLGA system
-        x = build.PLGA_system(75, 30, 1.7, 'ester', 2)
+        x = demo.PLGA_system(75, 30, 1.7, 'ester', 2)
         x.generate_conformers()
         # Calculate box components
         molecules, number_of_copies, topology, box_vectors, residual_monomer_actual = build.calculate_box_components(chains = x.chains,
--- swiftpol/tests/parameterize_tests.py
+++ swiftpol/tests/parameterize_tests.py
@@ -3,6 +3,8 @@ from rdkit import Chem
 from swiftpol.parameterize import charge_polymer, forcefield_with_charge_handler, forcefield_with_residualmonomer
 from openff.toolkit.typing.engines.smirnoff import ForceField
 from swiftpol import build 
+from swiftpol import demo
+from rdkit.Chem import AllChem
 
 class TestParameterize(unittest.TestCase):
     def test_charge_polymer(self):
@@ -20,6 +22,22 @@ class TestParameterize(unittest.TestCase):
         # Test invalid charge scheme
         with self.assertRaises(AttributeError):
             charges = charge_polymer(polymer, 'invalid')
+        
+        #Test with polymer build using build
+        sequence = 'ABAB'
+        monomer_a_smiles = 'OC(=O)CO'
+        monomer_b_smiles = 'C[C@@H](C(=O)[OH])O'
+        reaction = AllChem.ReactionFromSmarts('[C:1][HO:2].[HO:3][C:4]>>[C:1][O:2][C:4].[O:3]')
+        polymer, A_ratio, B_ratio = build.build_linear_copolymer(sequence, monomer_a_smiles, monomer_b_smiles, reaction)
+        # Test AM1_BCC charge scheme
+        charges = charge_polymer(polymer, 'AM1_BCC')
+        self.assertIsNotNone(charges)
+        # Test espaloma charge scheme
+        charges = charge_polymer(polymer, 'espaloma')
+        self.assertIsNotNone(charges)
+        # Test NAGL charge scheme
+        charges = charge_polymer(polymer, 'NAGL')
+        self.assertIsNotNone(charges)
 
     def test_forcefield_with_charge_handler(self):
         # Create a test molecule
@@ -28,11 +46,11 @@ class TestParameterize(unittest.TestCase):
         forcefield = forcefield_with_charge_handler(molecule, 'NAGL', ensemble=False)
         self.assertIsNotNone(forcefield)
         # Test with ensemble=True
-        ensemble_object = build.PLGA_system(80, 50, 1.0, 'ester', 5)
+        ensemble_object = demo.PLGA_system(80, 50, 1.0, 'ester', 5)
         forcefield = forcefield_with_charge_handler(ensemble_object, 'NAGL', ensemble=True)
         self.assertIsNotNone(forcefield)
         # Test with ensemble likely to contain duplicates
-        ensemble_object = build.PLGA_system(80, 50, 1.0, 'ester', 100)
+        ensemble_object = demo.PLGA_system(80, 50, 1.0, 'ester', 100)
         forcefield = forcefield_with_charge_handler(ensemble_object, 'NAGL', ensemble=True)
         self.assertIsNotNone(forcefield)
 
