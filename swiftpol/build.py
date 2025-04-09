97735259c518933cb4aeeb8a39a75f809725d5be
hannaomi <hannahturney0@gmail.com>
2025-04-09 16:51:40 +0100

    missing import in build

--- swiftpol/build.py
+++ swiftpol/build.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import time
+from collections import Counter
 try:
     from openeye import oechem
     oechem_imported = True
