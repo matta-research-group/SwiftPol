d51d639bcfb144b9c64cb4db37655eeff1c4f852
hannaomi <hannahturney0@gmail.com>
2025-08-28 15:45:59 +0100

    fix typo in build.py

--- swiftpol/build.py
+++ swiftpol/build.py
@@ -808,7 +808,7 @@ class polymer_system:
                             reaction=reaction,
                             terminal=terminals,
                             chain_num=n + 1,
-                            chainID=id_newd,
+                            chainID=id_new,
                         )
                     lengths.append(int(length_actual))
                     chains_rdkit.append(pol)
