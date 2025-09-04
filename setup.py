76628c7a8424f9d94fc2fe9991afe89c98b790b2
hannaomi <hannahturney0@gmail.com>
2025-09-04 13:30:22 +0100

    update setup.py version to 0.1.4

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 # Package setup
 setup(
     name='swiftpol',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(include=['swiftpol', 'swiftpol.*']),
 )
